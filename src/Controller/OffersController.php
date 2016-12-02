eb5f645e83edb8f490889a399588ab14d8319e7e
Anthony Gourd <anthonygourd@gmail.com>
2016-12-02 07:24:34 +0100

    Add an offert quick fix

--- src/Controller/OffersController.php
+++ src/Controller/OffersController.php
@@ -49,7 +49,7 @@ class OffersController extends AppController
      *
      * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
      */
-    public function add()
+    public function add($user_id)
     {
         $offer = $this->Offers->newEntity();
         if ($this->request->is('post')) {
@@ -57,13 +57,14 @@ class OffersController extends AppController
             if ($this->Offers->save($offer)) {
                 $this->Flash->success(__('The offer has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
+                return $this->redirect(['controller' => 'Users', 'action' => 'view', $offer->user_id]);
             } else {
                 $this->Flash->error(__('The offer could not be saved. Please, try again.'));
             }
         }
-        $users = $this->Offers->Users->find('list', ['limit' => 200]);
+
         $items = $this->Offers->Items->find('list', ['limit' => 200]);
+        $offer->user_id = $user_id;
         $this->set(compact('offer', 'users', 'items'));
         $this->set('_serialize', ['offer']);
     }
@@ -132,7 +133,7 @@ class OffersController extends AppController
                     return true;
                 }
             }
-            
+
             if(in_array($this->request->action, ['view']))
             {
                 return true;
